b0af444407be2a0a5ac76d5624bd186fec7215f4
Rayhan Hossain <hossain.rayhan@outlook.com>
2025-07-22 11:34:45 -0700

    Show CoonectionString Status in kubectl command (#55)
    Signed-off-by: Rayhan Hossain <rhossain@microsoft.com>
    Co-authored-by: Rayhan Hossain <rhossain@microsoft.com>

--- api/preview/documentdb_types.go
+++ api/preview/documentdb_types.go
@@ -72,6 +72,7 @@ type DocumentDBStatus struct {
 }
 
 // +kubebuilder:printcolumn:name="Status",type=string,JSONPath=".status.status",description="CNPG Cluster Status"
+// +kubebuilder:printcolumn:name="Connection String",type=string,JSONPath=".status.connectionString",description="DocumentDB Connection String"
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 
